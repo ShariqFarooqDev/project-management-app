8d7adaec309fa9d2d3668c0954c875731e2f8c2b
Shariq Farooq <shariqfarooq921@gmail.com>
2025-08-06 21:42:13 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 // client/src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <div>
-      <h1>Project Management App</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Project Management App</h1>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<DashboardPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
